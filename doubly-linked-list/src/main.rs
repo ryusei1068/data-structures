60face022da4a7e50154885e88f7f00a6b710087
Ryusei Nohara <nhrryi@gmail.com>
2024-05-06 20:29:06 +0900

    push_back

---
+++ doubly-linked-list/src/main.rs
@@ -0,0 +1,70 @@
+use std::{cell::RefCell, fmt, rc::Rc};
+
+#[derive(Clone)]
+struct Node<T: Clone> {
+    data: T,
+    next: Option<Rc<RefCell<Node<T>>>>,
+    prev: Option<Rc<RefCell<Node<T>>>>,
+}
+
+struct DoublyLinkedList<T: Clone> {
+    head: Option<Rc<RefCell<Node<T>>>>,
+    tail: Option<Rc<RefCell<Node<T>>>>,
+}
+
+impl<T: Clone> DoublyLinkedList<T> {
+    fn new() -> Self {
+        DoublyLinkedList {
+            head: None,
+            tail: None,
+        }
+    }
+
+    fn push_back(&mut self, data: T) {
+        let node = Rc::new(RefCell::new(Node {
+            data,
+            next: None,
+            prev: None,
+        }));
+
+        if self.head.is_none() {
+            self.head = Some(node.clone());
+            self.tail = Some(node);
+        } else {
+            node.as_ref().borrow_mut().prev.clone_from(&self.tail);
+            if let Some(ref mut tail) = self.tail {
+                tail.as_ref().borrow_mut().next = Some(node.clone());
+            }
+            self.tail = Some(node);
+        }
+    }
+}
+
+impl<T: Clone> fmt::Display for DoublyLinkedList<T>
+where
+    T: fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut current = self.head.clone();
+        while let Some(ref node) = current {
+            let n = node.as_ref().borrow().clone();
+            write!(f, " data: {}", n.data)?;
+            write!(f, " @{:p} ", &n)?;
+            current = n.next;
+            if current.is_some() {
+                write!(f, "<--->")?;
+            }
+        }
+        Ok(())
+    }
+}
+
+fn main() {
+    let mut list = DoublyLinkedList::new();
+
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+
+    println!("{}", list); // 1<--->2<--->3
+}
