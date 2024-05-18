08fa5ff42ef3f1a478becff205a19652555dd485
Ryusei Nohara <nhrryi@gmail.com>
2024-05-18 13:18:22 +0900

    use option

--- binary-tree/src/main.rs
+++ binary-tree/src/main.rs
@@ -1,21 +1,18 @@
+use std::cell::RefCell;
 use std::fmt;
+use std::rc::Rc;
 
-struct TreeNode<T: Copy> {
+struct TreeNode<T> {
     element: T,
-    left: BinaryTree<T>,
-    right: BinaryTree<T>,
+    left: Option<Rc<RefCell<TreeNode<T>>>>,
+    right: Option<Rc<RefCell<TreeNode<T>>>>,
 }
 
-enum BinaryTree<T: Copy> {
-    Empty,
-    NonEmpty(Box<TreeNode<T>>),
+struct BinarySearchTree<T> {
+    root: Option<Rc<RefCell<TreeNode<T>>>>,
 }
 
-struct BinarySearchTree<T: Copy> {
-    root: BinaryTree<T>,
-}
-
-impl<T: Ord + Copy> BinarySearchTree<T> {
+impl<T: Copy + Ord> BinarySearchTree<T> {
     fn new(mut arr: Vec<T>) -> Self {
         arr.sort();
         BinarySearchTree {
@@ -23,71 +20,53 @@ impl<T: Ord + Copy> BinarySearchTree<T> {
         }
     }
 
-    fn sorted_bst(arr: &Vec<T>, start: usize, end: usize) -> BinaryTree<T> {
+    fn sorted_bst(arr: &Vec<T>, start: usize, end: usize) -> Option<Rc<RefCell<TreeNode<T>>>> {
         if start == end {
-            return BinaryTree::NonEmpty(Box::new(TreeNode {
+            return Some(Rc::new(RefCell::new(TreeNode {
                 element: arr[start],
-                left: BinaryTree::Empty,
-                right: BinaryTree::Empty,
-            }));
+                left: None,
+                right: None,
+            })));
         }
 
         let mid = (start + end) / 2;
-        let mut left = BinaryTree::Empty;
+        let mut left = None;
         if start < mid {
             left = Self::sorted_bst(arr, start, mid - 1);
         }
-        let mut right = BinaryTree::Empty;
+        let mut right = None;
         if end > mid {
             right = Self::sorted_bst(arr, mid + 1, end);
         }
 
-        BinaryTree::NonEmpty(Box::new(TreeNode {
+        Some(Rc::new(RefCell::new(TreeNode {
             element: arr[mid],
             left,
             right,
-        }))
+        })))
     }
 
     fn insert(&mut self, value: T) {
-        self.root.add(value);
-    }
-
-    fn search(&self, key: T) -> Option<&TreeNode<T>> {
-        self.root.search(key)
-    }
-}
-
-impl<T: Ord + Copy> BinaryTree<T> {
-    fn add(&mut self, value: T) {
-        match *self {
-            BinaryTree::Empty => {
-                *self = BinaryTree::NonEmpty(Box::new(TreeNode {
+        let mut iterator = self.root.clone();
+        while let Some(node) = iterator {
+            if node.borrow().left.clone().is_none() && node.borrow().element > value {
+                node.borrow_mut().left = Some(Rc::new(RefCell::new(TreeNode {
                     element: value,
-                    left: BinaryTree::Empty,
-                    right: BinaryTree::Empty,
-                }))
+                    left: None,
+                    right: None,
+                })))
+            } else if node.borrow().right.clone().is_none() && node.borrow().element < value {
+                node.borrow_mut().right = Some(Rc::new(RefCell::new(TreeNode {
+                    element: value,
+                    left: None,
+                    right: None,
+                })))
             }
-            BinaryTree::NonEmpty(ref mut node) => {
-                if value <= node.element {
-                    node.left.add(value)
-                } else {
-                    node.right.add(value)
-                }
-            }
-        }
-    }
-
-    fn search(&self, value: T) -> Option<&TreeNode<T>> {
-        use std::cmp::Ordering;
-
-        match *self {
-            BinaryTree::NonEmpty(ref node) => match node.element.cmp(&value) {
-                Ordering::Greater => node.left.search(value),
-                Ordering::Less => node.right.search(value),
-                Ordering::Equal => Some(node),
-            },
-            BinaryTree::Empty => None,
+            iterator = if node.borrow().element > value {
+                node.borrow().left.clone()
+            } else {
+                node.borrow().right.clone()
+            };
         }
     }
 }
@@ -97,39 +76,26 @@ where
     T: fmt::Display,
 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.root.fmt(f)?;
-        Ok(())
-    }
-}
-
-impl<T: Copy> fmt::Display for BinaryTree<T>
-where
-    T: fmt::Display,
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use std::collections::VecDeque;
 
         let mut queue = VecDeque::new();
-        queue.push_back(self);
+        if let Some(root) = self.root.clone() {
+            queue.push_back(root);
+        }
 
         while let Some(cur_node) = queue.pop_front() {
-            match *cur_node {
-                BinaryTree::NonEmpty(ref cur_node) => {
-                    write!(f, " {} @{:p} ", cur_node.element, &cur_node.element)?;
-                    match cur_node.left {
-                        BinaryTree::NonEmpty(_) => {
-                            queue.push_back(&cur_node.left);
-                        }
-                        BinaryTree::Empty => {}
-                    }
-                    match cur_node.right {
-                        BinaryTree::NonEmpty(_) => {
-                            queue.push_back(&cur_node.right);
-                        }
-                        BinaryTree::Empty => {}
-                    }
-                }
-                BinaryTree::Empty => {}
+            write!(
+                f,
+                " {} @{:p} ",
+                cur_node.borrow().element,
+                &cur_node.borrow().element
+            )?;
+
+            if let Some(node_left) = cur_node.borrow().left.clone() {
+                queue.push_back(node_left)
+            }
+            if let Some(node_right) = cur_node.borrow().right.clone() {
+                queue.push_back(node_right)
             }
         }
 
@@ -152,15 +118,15 @@ fn main() {
     //            2          8
     //          ↙︎  ↘︎      ↙︎    ↘︎
     //         1   3     6      9
-    //              ↘︎     ↘︎       ↘︎ 
+    //              ↘︎     ↘︎       ↘︎
     //              4     7       10
     let arr = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let mut bst = BinarySearchTree::new(arr);
     println!("{}", bst);
 
-    if let Some(node) = bst.search(8) {
-        println!("\nbinary search: {}\n", node.element);
-    }
+    // if let Some(node) = bst.search(8) {
+    //     println!("\nbinary search: {}\n", node.element);
+    // }
 
     bst.insert(0);
     bst.insert(11);
